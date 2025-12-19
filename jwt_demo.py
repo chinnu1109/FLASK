452a2e9696d3839bbbe720fa16f027cf76a757b5
chinnu <chinnu@gmail.com>
2025-12-19 12:51:42 +0530

    added hiiii in jwt_demo.py in aboutuse route

--- jwt_demo.py
+++ jwt_demo.py
@@ -38,8 +38,9 @@ def about():
         "definition": "JWT is an open standard (RFC 7519) that defines a compact and self-contained way for securely transmitting information between parties as a JSON object.",
         "how_it_works": "Information can be verified and trusted because it is digitally signed. JWTs can be signed using a secret (with the HMAC algorithm) or a public/private key pair using RSA or ECDSA.",
         "why_use_it": "It's compact, self-contained, and works across different programming languages, making it ideal for stateless authentication in modern web applications."
+        "hiiiiii":"hiiiiiii"
     }), 200
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) 
